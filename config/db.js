88c5c2fd80f6ea7fbf20b2a45694f50537bc3480
Matthew Egan <mnegan93@gmail.com>
2021-10-11 11:41:03 +1300

    Renamed db to dbConnectionString
    Renamed variable to be more specific.

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const config = require('config')
-const db = config.get('mongoURI')
+const dbConnectionString = config.get('mongoURI')
 
 const connectDB = async () => {
   try {
     await mongoose
-    .connect(db, {
+    .connect(dbConnectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
