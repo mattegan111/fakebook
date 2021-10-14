272852cad38d99477ced456f0c93589b1be78078
Matthew Egan <mnegan93@gmail.com>
2021-10-14 17:17:10 +1300

    Encrypt User password using bcryptjs

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const {
 	body,
 	validationResult,
@@ -25,7 +26,7 @@ router.post(
 			'Please include a password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res
@@ -33,7 +34,36 @@ router.post(
 				.json({ errors: errors.array() });
 		}
 
-		res.send('passed');
+		const { firstname, lastname, email, password } =
+			req.body;
+
+		try {
+			let user = await User.findOne({ email: email });
+
+			if (user) {
+				return res
+					.status(400)
+					.json({ msg: 'Email already in use' });
+			}
+
+			user = new User({
+				firstname,
+				lastname,
+				email,
+				password,
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			res.send('user saved');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
