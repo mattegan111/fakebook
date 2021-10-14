bd2249b37758618b03279e6a48b26ae90cd76252
Matthew Egan <mnegan93@gmail.com>
2021-10-14 16:06:38 +1300

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+	firstname: {
+		type: String,
+		required: true,
+	},
+	lastname: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	bio: {
+		type: String,
+	},
+	friends: {
+		type: Array,
+	},
+	notifications: {
+		type: Array,
+	},
+	receivedfriendrequests: {
+		type: Array,
+	},
+	sentfriendrequests: {
+		type: Array,
+	},
+	post: {
+		type: Array,
+	},
+	comments: {
+		type: Array,
+	},
+});
+
+UserSchema.virtual('fullname').get(() => {
+	return this.firstname + ' ' + this.lastname;
+});
+
+module.exports = mongoose.model('user', UserSchema);
