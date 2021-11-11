d079a2247244876d5f2b3fb08d2dca5be6ff11ee
Matthew Egan <mnegan93@gmail.com>
2021-11-12 09:42:31 +1300

    Introduce initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
+import TimelinePage from './components/pages/TimelinePage';
+import ProfilePage from './components/pages/ProfilePage';
+import './App.css';
+
 
 const App = () => {
 	return (
 		<div className='App'>
-			<Navbar />
-			My App
+			<Router>
+				<Navbar />
+				<Route exact path='/'>
+					<TimelinePage />
+				</Route>
+				<Route path='/:id'>
+					<ProfilePage />
+				</Route>
+			</Router>
 		</div>
 	);
 }
